f98ac0d03f8fe9ae0de7d757b5f9d35bfa46fd6a
delannyo <yoan.delannoy1@gmail.com>
2024-09-30 17:52:04 +0200

    Change nav-bar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,86 +1,71 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { useContext } from "react";
-import { PageContext } from "../../utils/context";
+import style from './Header.module.css'
+import {Link} from "react-router-dom";
+import {useContext, useRef} from "react";
+import {PageContext} from "../../utils/context";
 
-const HeaderWrap = styled.header`
-  border: 1px white solid;
-  position: fixed;
-  top: 40px;
-  left: 50%;
-  transform: translate(-50%);
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  border-radius: 20px;
-  z-index: 1000;
-`;
-
-const StyledLink = styled(Link)`
-    padding: 10px 0;
-    width: 180px;
-    text-align: center;
-    text-decoration: none;
-    color: white;
-    background-color: black;
-    transition: background-color 0.3s ease;
-
-    &:hover {
-        background-color: blueviolet;
-        color: white;
-    }
-
-    &.active {
-        background-color: blueviolet;
-        color: white;
-    }
-
-    &:first-child {
-        border-radius: 20px 0 0 20px;
-    }
-
-    &:last-child {
-        border-radius: 0 20px 20px 0;
-    }
-`;
-
-function Header() {
+function NewHeader() {
     const { page, changePage } = useContext(PageContext);
+    const checkboxRef = useRef(null);  // Référence pour la checkbox
+
+    // Fonction pour décocher la checkbox
+    const handleLinkClick = (pageName) => {
+        changePage(pageName);  // Met à jour la page
+        if (checkboxRef.current) {
+            checkboxRef.current.checked = false;  // Décocher la checkbox
+        }
+    };
 
     return (
-        <HeaderWrap>
-            <StyledLink
-                to="/"
-                className={page === 'home' ? 'active' : ''}
-                onClick={() => changePage('home')}
-            >
-                Accueil
-            </StyledLink>
-
-            <StyledLink
-                to="/skills"
-                className={page === 'skills' ? 'active' : ''}
-                onClick={() => changePage('skills')}
-            >
-                Compétences
-            </StyledLink>
-            <StyledLink
-                to="/projets"
-                className={page === 'projets' ? 'active' : ''}
-                onClick={() => changePage('projets')}
-            >
-                Projets
-            </StyledLink>
-
-            <StyledLink
-                to="/experiences"
-                className={page === 'experiences' ? 'active' : ''}
-                onClick={() => changePage('experiences')}
-            >
-                Expériences
-            </StyledLink>
-        </HeaderWrap>
+        <div>
+            <input
+                className={style.menuIcon}
+                type="checkbox"
+                id="menu-icon"
+                ref={checkboxRef}  // Utiliser la référence ici
+            />
+            <label htmlFor="menu-icon" className={style.menuLabel}/>
+            <nav className={style.nav}>
+                <ul className={style.navList}>
+                    <li>
+                        <Link
+                            to="/"
+                            className={page === 'home' ? 'active' : ''}
+                            onClick={() => handleLinkClick('home')}  // Appel à la fonction lors du clic
+                        >
+                            Accueil
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/skills"
+                            className={page === 'skills' ? 'active' : ''}
+                            onClick={() => handleLinkClick('skills')}
+                        >
+                            Compétences
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/projets"
+                            className={page === 'projets' ? 'active' : ''}
+                            onClick={() => handleLinkClick('projets')}
+                        >
+                            Projets
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/experiences"
+                            className={page === 'experiences' ? 'active' : ''}
+                            onClick={() => handleLinkClick('experiences')}
+                        >
+                            Expériences
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 
-export default Header;
+export default NewHeader;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React, {useContext, useEffect, useRef, useState} from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
-import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import GlobalStyle from "./utils/style/GlobalStyle";
@@ -9,7 +8,7 @@ import Experiences from "./pages/Experiences";
 import Projets from "./pages/Projets";
 import Error from "./pages/Error";
 import Skills from "./pages/Skills";
-import FusionGraphe from "./pages/projets/FusionGraphe"
+import FusionGraphe from "./pages/projets/FusionGraphe";
 import RobotGravity from "./pages/projets/RobotGravity";
 import OldPortfolio from "./pages/projets/OldPortfolio";
 import GADI from "./pages/projets/GADI";
@@ -18,64 +17,37 @@ import CarnetStage from "./pages/projets/CarnetStage";
 import APINews from "./pages/projets/API-news";
 import Crescendo from "./pages/projets/Crescendo";
 import EpsilonManager from "./pages/projets/EpsilonManager";
-import {PageContext, PageProvider} from "./utils/context";
-
-// Ordre des pages
-const pageOrder = ['home', 'skills', 'projets', 'experiences'];
+import { PageProvider } from "./utils/context";
 
 function App() {
     const location = useLocation();
-    const { page } = useContext(PageContext);  // Page actuelle
-    const prevPageRef = useRef(page);  // Utilise useRef pour mémoriser la page précédente
-    const [transitionDirection, setTransitionDirection] = useState('left');
 
     useEffect(() => {
-        const currentPageIndex = pageOrder.indexOf(page);
-        const prevPageIndex = pageOrder.indexOf(prevPageRef.current);
-
-        // Détermine la direction de la transition
-        if (currentPageIndex > prevPageIndex) {
-            setTransitionDirection('right'); // Vers la droite
-        } else {
-            setTransitionDirection('left'); // Vers la gauche
-        }
-
-        prevPageRef.current = page; // Met à jour la page précédente dans le ref sans déclencher de re-render
-
         // Ramène la vue vers le haut de la page
         window.scrollTo(0, 0);
-    }, [location, page]);
+    }, [location]); // Ajoute location comme dépendance pour scroll sur chaque changement de page
 
     return (
         <>
             <GlobalStyle />
             <div className="rotating-background"></div>
             <Header />
-            <SwitchTransition>
-                <CSSTransition
-                    key={location.pathname} // Clé basée sur la route pour déclencher la transition
-                    classNames={`slide-${transitionDirection}`}
-                    timeout={300}
-                    unmountOnExit
-                >
-                    <Routes location={location}>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/experiences" element={<Experiences />} />
-                        <Route path="/projets" element={<Projets />} />
-                        <Route path="/projets/fusion-graphe" element={<FusionGraphe />} />
-                        <Route path="/projets/robot-gravity" element={<RobotGravity />} />
-                        <Route path="/projets/old-portfolio" element={<OldPortfolio />} />
-                        <Route path="/projets/gadi" element={<GADI />} />
-                        <Route path="/projets/tour-of-heroes" element={<TourOfHeroes />} />
-                        <Route path="/projets/carnet-stage" element={<CarnetStage />} />
-                        <Route path="/projets/api-news" element={<APINews />} />
-                        <Route path="/projets/crescendo" element={<Crescendo />} />
-                        <Route path="/projets/epsilon-manager" element={<EpsilonManager />} />
-                        <Route path="/skills" element={<Skills />} />
-                        <Route path="*" element={<Error />} />
-                    </Routes>
-                </CSSTransition>
-            </SwitchTransition>
+            <Routes location={location}>
+                <Route path="/" element={<Home />} />
+                <Route path="/experiences" element={<Experiences />} />
+                <Route path="/projets" element={<Projets />} />
+                <Route path="/projets/fusion-graphe" element={<FusionGraphe />} />
+                <Route path="/projets/robot-gravity" element={<RobotGravity />} />
+                <Route path="/projets/old-portfolio" element={<OldPortfolio />} />
+                <Route path="/projets/gadi" element={<GADI />} />
+                <Route path="/projets/tour-of-heroes" element={<TourOfHeroes />} />
+                <Route path="/projets/carnet-stage" element={<CarnetStage />} />
+                <Route path="/projets/api-news" element={<APINews />} />
+                <Route path="/projets/crescendo" element={<Crescendo />} />
+                <Route path="/projets/epsilon-manager" element={<EpsilonManager />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
         </>
     );
 }
